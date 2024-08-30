df8126be9255549d65fad9f70c33963745825b2c
dfkdream <dfkdream@gmail.com>
2024-08-30 16:57:25 +0900

    Add SvelteKitPWA vite configurations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
     import '../app.css';
     import { App } from 'konsta/svelte';
+    import { pwaInfo } from 'virtual:pwa-info';
+
+    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
 <svelte:head>
+    {@html webManifestLink}
     <title>SvelteKit PWA</title>
 </svelte:head>
   
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,27 @@ import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA()
+		SvelteKitPWA({
+			strategies: "injectManifest",
+			srcDir: "src",
+			filename: "service-worker.js",
+			injectRegister: null,
+			manifest: {
+				name: "sveltekit-pwa",
+				short_name: "sveltekit-pwa",
+				description: "SvelteKit template for Progressive Web App",
+				background_color: "rgb(247 247 248)",
+				theme_color: "rgb(247 247 248)",
+				lang: "en-US",
+				icons: [
+					{
+						src: "favicon.png",
+						sizes: "128x128",
+						type: "image/png",
+						purpose: "any maskable"
+					}
+				]
+			},
+		})
 	]
 });
